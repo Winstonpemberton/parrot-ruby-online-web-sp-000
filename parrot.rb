3f30769159c8723ff4f7b7ccdc15a3d16c96a464
Winston Pemberton <winstonpemberton@gmail.com>
2019-11-05 22:47:50 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(input, speak = "Squawk")
-  puts "#{input}"
+def parrot(speak = "Squawk")
+  puts "#{speak}"
   speak
 end
